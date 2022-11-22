0d8e56ac3847d17d9ec89182e9a447e11ec473a9
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-22 10:18:50 +0100

    initial commit

---
+++ src/main/java/michalmlynarczyk/studentteachercrud/StudentTeacherCrudApplication.java
@@ -0,0 +1,13 @@
+package michalmlynarczyk.studentteachercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTeacherCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTeacherCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/michalmlynarczyk/studentteachercrud/StudentTeacherCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package michalmlynarczyk.studentteachercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTeacherCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
