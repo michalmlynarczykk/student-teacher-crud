2cc62f1cd20e7402eb02831871bc7679a309b72f
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-23 11:17:48 +0100

    add: model mapper bean

--- src/main/java/michalmlynarczyk/studentteachercrud/StudentTeacherCrudApplication.java
+++ src/main/java/michalmlynarczyk/studentteachercrud/StudentTeacherCrudApplication.java
@@ -1,13 +1,19 @@
 package michalmlynarczyk.studentteachercrud;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudentTeacherCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentTeacherCrudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentTeacherCrudApplication.class, args);
+    }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
