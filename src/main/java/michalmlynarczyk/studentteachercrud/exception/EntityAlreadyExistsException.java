e0d202223faca25aa17d4a24a7fb87bba7573c86
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-24 11:53:11 +0100

    refactor: get rid of unused methods

--- src/main/java/michalmlynarczyk/studentteachercrud/exception/EntityAlreadyExistsException.java
+++ src/main/java/michalmlynarczyk/studentteachercrud/exception/EntityAlreadyExistsException.java
@@ -4,8 +4,4 @@ public class EntityAlreadyExistsException extends RuntimeException {
     public EntityAlreadyExistsException(String message) {
         super(message);
     }
-
-    public EntityAlreadyExistsException(Throwable cause) {
-        super(cause);
-    }
 }
